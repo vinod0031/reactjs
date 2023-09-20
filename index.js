7a0ffc8aa6c608eafd88c451bb4104f2c306c72e
vinod0031 <kvinodkumar0031@gmail.com>
2023-09-20 07:13:26 +0000

    commit message

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  //   Write your code here.
+
+  <div className="backgroundcolor">
+    <h1>Super Over League</h1>
+    <div>
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+      <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
